642c17778085b3453ef04ba5d11bee9a018fca39
DJED90 <djed.houbib@gmail.com>
2023-08-07 17:03:26 +0200

    Modification des elements avec sass

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import "./style.sass";
 function template_creation(){
     const container = document.querySelector("#app");
+    console.log(container);
+    
+    console.log(ul);
 
 
 // On récupère le template souhaité
 const template = document.querySelector("#base-template");
-console.log(template);
+const task_template = document.querySelector("#task-template");
+
 
 // On clone le template 
 const clone = document.importNode(template.content,true);
+const clone_task = document.importNode(task_template.content,true)
 
 //On va chercher les balises de la div "entete"//
 const entete = clone.querySelector("#entete");
+const ul =  clone.querySelector("#todo_task");
 
 
 //On inject le contenu souhaité
 container.appendChild(clone);
+ul.appendChild(clone_task);
 }
 template_creation();
