d19ef02f8c537fba40b8563af7ece90e5095ab07
DJED90 <djed.houbib@gmail.com>
2023-09-12 08:50:45 +0200

    final commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./style.sass";
 import"./addtask.sass";
 import "./darkmode.sass";
 import {template_creation} from "./template_creation";
-import { addtask,changetask,removetask } from "./addtask";
+import { addtask,changetask,removetask, renametask } from "./addtask";
 import { menu } from "./menu";
 import { toggleDarkMode } from "./darkmode";
 import { draganddrop} from "./drag-and-drop";
@@ -16,5 +16,6 @@ export function importfunction(){
     draganddrop();
     search();
     changetask();
+    renametask();
 }
 importfunction();
